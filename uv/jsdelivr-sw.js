4ae47f0466c1f8442e52413aea798bbc9e017e68
zaka13alt <zakariyaliban719@gmail.com>
2026-07-08 15:51:13 -0500

    Add service worker registration in jsdelivr-sw.js

---
+++ uv/jsdelivr-sw.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        console.log('Ultraviolet Service Worker registered');
+    }).catch((err) => {
+        console.error('Service Worker registration failed:', err);
+    });
+}
